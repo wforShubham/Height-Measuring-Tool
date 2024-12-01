b0e3e120e5adfe6acfb50bdb9192a418848bedcd
Shubham Vishwakarma <94854000+wforShubham@users.noreply.github.com>
2024-12-02 04:05:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function main() {
+    // Check if device orientation is supported
+    if (window.DeviceOrientationEvent) {
+        window.addEventListener("deviceorientation", onOrientationChange);
+    } else {
+        alert("Device orientation is not supported on this device.");
+    }
+
+    // Access video stream
+    navigator.mediaDevices.getUserMedia({ video: {
+        facingMode:'environment'
+    } })
+        .then(function(signal) {
+            const video = document.getElementById("myVideo");
+            video.srcObject = signal;
+            video.play();  // Start video playback
+        })
+        .catch(function(err) {
+            alert(err);
+        });
+}
+
+function onOrientationChange(event) {
+    console.log(event.beta);
+    let angle = event.beta - 90;
+    if (angle < 0) {
+        angle = 0;
+    }
+
+    const distToTree = document.getElementById("mySlider").value;
+    document.getElementById("myLabel").innerHTML = "Distance to object: " + distToTree + " meters";
+    
+    const height = Math.tan(angle * Math.PI / 180) * distToTree;
+    document.getElementById("heightInfo").innerHTML = height.toFixed(1) + " m (" + angle.toFixed(1) + "&deg;)";
+}
